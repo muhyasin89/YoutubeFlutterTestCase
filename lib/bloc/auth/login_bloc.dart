3dc3a1f0f7cbc6671c457c878db8927f631e4a6c
muhyasin89 <muhyasin89@gmail.com>
2022-11-06 13:56:09 +0700

    [bloc/auth/1]add final

--- lib/bloc/auth/login_bloc.dart
+++ lib/bloc/auth/login_bloc.dart
@@ -1,7 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube_flutter_test_case/bloc/auth/login_event.dart';
 import 'package:youtube_flutter_test_case/bloc/auth/login_state.dart';
+import 'package:youtube_flutter_test_case/bloc/auth_repository.dart';
+import 'package:youtube_flutter_test_case/bloc/form_submission_status.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginState());
+  final AuthRepository authRepo;
+  LoginBloc({required this.authRepo}) : super(LoginState());
+
+  @override
+  Stream<LoginState> mapEventToState(Login event) async* {
+    if (event is LoginUsernameChange) {
+      yield state.copyWith(username: event.username);
+    } else if (event is LoginPasswordChange) {
+      yield state.copyWith(password: event.password);
+    }else if(event is LoginSubmitted){
+      yield state.copyWith(formStatus: FormSubmitting())
+    }
+
+    try{
+      await authRepo.login();
+      yield state.copyWith(formStatus: SubmissionSuccess());
+    }catch (e){
+      yield state.copyWith(formStatus: SubmissionFailed(exception: e));
+    }
+  }
 }
--- lib/bloc/auth/login_state.dart
+++ lib/bloc/auth/login_state.dart
@@ -1,10 +1,15 @@
+import 'package:youtube_flutter_test_case/bloc/form_submission_status.dart';
+
 class LoginState {
   final String username;
   final String password;
 
   LoginState({this.username = '', this.password = ''});
 
-  LoginState copyWith({String? username, String? password}) {
+  LoginState copyWith({
+    String? username,
+    String? password,
+  }) {
     return LoginState(
         username: username ?? this.username,
         password: password ?? this.password);
