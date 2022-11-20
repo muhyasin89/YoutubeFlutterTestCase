f737644ce983d330cbddc2cb7318646b30ba9206
muhyasin89 <muhyasin89@gmail.com>
2022-11-20 14:47:40 +0700

    [bloc/login/2]add login

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
-import './screen/home_screen.dart';
+import 'package:youtube_flutter_test_case/repository/authentication_repository.dart';
+import 'package:youtube_flutter_test_case/app.dart';
+import 'repository/user_repository.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MyHomeScreen());
-  }
+  runApp(
+    App(
+      authenticationRepository: AuthenticationRepository(),
+      userRepository: UserRepository(),
+    ),
+  );
 }
